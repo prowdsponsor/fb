37c3a30af95279d49e24b10d31df7b90356a554d
paulrzcz <paul@paulrzcz>
2013-05-06 07:53:52 +0200

    DeTABed identation

--- src/Facebook/Object/Order.hs
+++ src/Facebook/Object/Order.hs
@@ -1,10 +1,11 @@
 {-# LANGUAGE DeriveDataTypeable, FlexibleContexts, OverloadedStrings #-}
 module Facebook.Object.Order
-	( Order(..)
-	, OrderStatus(..)
-	, OrderApplication(..)
-	, getOrder
-	) where
+    ( Order(..)
+    , OrderId
+    , OrderStatus(..)
+    , OrderApplication(..)
+    , getOrder
+    ) where
 
 import Control.Applicative
 import Control.Monad (mzero)
@@ -26,65 +27,65 @@ type OrderId = Id
 
 -- | A Facebook Order oject
 data Order = Order {
-	orderId   	:: OrderId,
-	orderFrom 	:: UserId,
-	orderTo	  	:: UserId,
-	orderAmount :: Integer,
-	orderStatus :: OrderStatus,
-	orderApplication 	:: OrderApplication,
-	orderCountry		:: Text,
-	orderRefundCode		:: Maybe Text,
-	orderCreatedTime 	:: ZonedTime,
-	orderUpdatedTime	:: ZonedTime
+    orderId             :: OrderId,
+    orderFrom           :: UserId,
+    orderTo	            :: UserId,
+    orderAmount         :: Integer,
+    orderStatus         :: OrderStatus,
+    orderApplication    :: OrderApplication,
+    orderCountry        :: Text,
+    orderRefundCode     :: Maybe Text,
+    orderCreatedTime    :: ZonedTime,
+    orderUpdatedTime    :: ZonedTime
 } deriving (Show, Typeable)
 
 -- | A Facebook Order status type
 data OrderStatus = 
-	OrderPlaced 
-	| OrderSettled 
-	| OrderRefunded 
-	| OrderDisputed 
-	| OrderCancelled
-	deriving (Show, Enum, Eq, Typeable)
+    OrderPlaced 
+    | OrderSettled 
+    | OrderRefunded 
+    | OrderDisputed 
+    | OrderCancelled
+    deriving (Show, Enum, Eq, Typeable)
 
 -- | A trimmed down version of Facebook Application as it is used in Order
 data OrderApplication = OrderApplication {
-	appId 	:: Text,
-	appName	:: Text
+    appId 	:: Text,
+    appName	:: Text
 } deriving (Show, Typeable)
 
 instance A.FromJSON OrderApplication where
-	parseJSON (A.Object v) 	=
-		OrderApplication 	<$> v .: "id"
-						 	<*> v .: "name"
-	parseJSON _ 			= mzero 
+    parseJSON (A.Object v)  =
+        OrderApplication    <$> v .: "id"
+                            <*> v .: "name"
+	parseJSON _             = mzero 
 
 instance A.FromJSON Order where
-	parseJSON (A.Object v) =
-		Order <$> v .: "id"
-			  <*> v .: "from"
-			  <*> v .: "to"
-			  <*> v .: "amount"
-			  <*> v .: "status"
-			  <*> v .: "application"
-			  <*> v .: "country"
-			  <*> v .:? "refund_reason_code"
-			  <*> v .: "created_time"
-			  <*> v .: "updated_time"
+    parseJSON (A.Object v) =
+        Order   <$> v .: "id"
+                <*> v .: "from"
+                <*> v .: "to"
+                <*> v .: "amount"
+                <*> v .: "status"
+                <*> v .: "application"
+                <*> v .: "country"
+                <*> v .:? "refund_reason_code"
+                <*> v .: "created_time"
+                <*> v .: "updated_time"
 	parseJSON _ = mzero
 
 instance A.FromJSON OrderStatus where
-	parseJSON (A.String "placed") 		= return OrderPlaced
-	parseJSON (A.String "settled") 		= return OrderSettled
-	parseJSON (A.String "refunded")		= return OrderRefunded
-	parseJSON (A.String "disputed")		= return OrderDisputed
-	parseJSON (A.String "cancelled")	= return OrderCancelled
-	parseJSON _							= mzero
+    parseJSON (A.String "placed")       = return OrderPlaced
+    parseJSON (A.String "settled")      = return OrderSettled
+    parseJSON (A.String "refunded")     = return OrderRefunded
+    parseJSON (A.String "disputed")     = return OrderDisputed
+    parseJSON (A.String "cancelled")    = return OrderCancelled
+    parseJSON _	                        = mzero
 
 -- | Get an order using order ID.  The user access token is
 -- mandatory. 
 getOrder :: (C.MonadResource m, MonadBaseControl IO m) =>
            OrderId         -- ^ Order ID.
-        -> UserAccessToken -- ^ Optional user access token.
+        -> UserAccessToken -- ^ User access token.
         -> FacebookT anyAuth m Order
 getOrder id_ mtoken = getObject ("/" <> idCode id_) [] (Just mtoken)
