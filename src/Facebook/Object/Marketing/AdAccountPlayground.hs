bd685feaaa371105cb7ef890090ce7fc53eee90e
Alex Gremm <alex@beautifuldestinations.com>
2015-12-11 15:37:31 +0000

    Fix FundingSourceDetail (ADT -> record), Add IsAdAccountField class + instances for (some) valid fields

--- src/Facebook/Object/Marketing/AdAccountPlayground.hs
+++ src/Facebook/Object/Marketing/AdAccountPlayground.hs
@@ -123,36 +123,36 @@ instance Field FId where
     fieldName _ = "id"
     fieldLabel = FId
 
-data FundingSourceDetails =
-      FS_Id
-    | FS_Coupon
-    | FS_Amount
-    | FS_Currency
-    | FS_DisplayAmount
-    | FS_Expiration
-    | FS_DisplayString
-    | FS_Type deriving Show
-
-instance A.FromJSON FundingSourceDetails where -- FIXME
-  parseJSON (A.Number 1) = return FS_Id
-  parseJSON (A.Number 2) = return FS_Coupon
-  parseJSON (A.Number 3) = return FS_Amount
-  parseJSON (A.Number 7) = return FS_Currency
-  parseJSON (A.Number 9) = return FS_DisplayAmount
-  parseJSON (A.Number 101) = return FS_Expiration
-  parseJSON (A.Number 100) = return FS_DisplayString
-  parseJSON (A.Number 102) = return FS_Type
-  parseJSON _ = mzero
-
-instance A.ToJSON FundingSourceDetails where
-  toJSON FS_Id = A.Number 1
-  toJSON FS_Coupon = A.Number 2
-  toJSON FS_Amount = A.Number 3
-  toJSON FS_Currency = A.Number 7
-  toJSON FS_DisplayAmount = A.Number 9
-  toJSON FS_Expiration = A.Number 101
-  toJSON FS_DisplayString = A.Number 100
-  toJSON FS_Type = A.Number 102
+--data FundingSourceDetails = -- this is not an enumeration, these are fields of a record!
+--      FS_Id
+--    | FS_Coupon
+--    | FS_Amount
+--    | FS_Currency
+--    | FS_DisplayAmount
+--    | FS_Expiration
+--    | FS_DisplayString
+--    | FS_Type deriving Show
+--
+--instance A.FromJSON FundingSourceDetails where -- FIXME
+--  parseJSON (A.Number 1) = return FS_Id
+--  parseJSON (A.Number 2) = return FS_Coupon
+--  parseJSON (A.Number 3) = return FS_Amount
+--  parseJSON (A.Number 7) = return FS_Currency
+--  parseJSON (A.Number 9) = return FS_DisplayAmount
+--  parseJSON (A.Number 101) = return FS_Expiration
+--  parseJSON (A.Number 100) = return FS_DisplayString
+--  parseJSON (A.Number 102) = return FS_Type
+--  parseJSON _ = mzero
+--
+--instance A.ToJSON FundingSourceDetails where
+--  toJSON FS_Id = A.Number 1
+--  toJSON FS_Coupon = A.Number 2
+--  toJSON FS_Amount = A.Number 3
+--  toJSON FS_Currency = A.Number 7
+--  toJSON FS_DisplayAmount = A.Number 9
+--  toJSON FS_Expiration = A.Number 101
+--  toJSON FS_DisplayString = A.Number 100
+--  toJSON FS_Type = A.Number 102
 
 data FSD = FSD
 instance Field FSD where
@@ -160,9 +160,37 @@ instance Field FSD where
     fieldName _ = "funding_source_details"
     fieldLabel = FSD
 
+type FundingSourceDetails = FSD_Id :*: FSD_Type :*: FSD_DisplayString :*: Nil
+data FSD_Id = FSD_Id
+instance Field FSD_Id where
+    type FieldValue FSD_Id = Id
+    fieldName _ = "id"
+    fieldLabel = FSD_Id
+
+data FSD_Type = FSD_Type
+instance Field FSD_Type where
+    type FieldValue FSD_Type = Int
+    fieldName _ = "type"
+    fieldLabel = FSD_Type
+
+data FSD_DisplayString = FSD_DisplayString
+instance Field FSD_DisplayString where
+    type FieldValue FSD_DisplayString = Text
+    fieldName _ = "display_string"
+    fieldLabel = FSD_DisplayString
+-- TODO: other fields
+--      FS_Id
+--    | FS_Coupon
+--    | FS_Amount
+--    | FS_Currency
+--    | FS_DisplayAmount
+--    | FS_Expiration
+--    | FS_DisplayString
+--    | FS_Type deriving Show
+
 data AdAccountGroups = AdAccountGroups
 instance Field AdAccountGroups where
-    type FieldValue AdAccountGroups = (V.Vector AAGP.AdAccountGroupResult) :*: Nil -- inner ext. record
+    type FieldValue AdAccountGroups = V.Vector AAGP.AdAccountGroupResult
     fieldName _ = "account_groups"
     fieldLabel = AdAccountGroups
 
@@ -174,7 +202,7 @@ instance Field FAdAccountId where
 
 data AdAccountAge = AdAccountAge
 instance Field AdAccountAge where
-    type FieldValue AdAccountAge = Float
+    type FieldValue AdAccountAge = Double
     fieldName _ = "age"
     fieldLabel = AdAccountAge
 
@@ -186,7 +214,7 @@ instance Field AdAccountACD where
 
 data AgencyClientDeclaration = AgencyClientDeclaration {
     agency_representing_client  :: Int } -- TODO
-    deriving Generic
+    deriving (Generic, Show)
 instance A.FromJSON AgencyClientDeclaration
 instance A.ToJSON AgencyClientDeclaration
 
@@ -215,17 +243,29 @@ instance Field AdAccountBalance where
     fieldLabel = AdAccountBalance
 
 -- requests... first character in lower case... hide the ugliness
+-- TODO: beautify!
 adAccountBalance :: (AdAccountBalance, Text)
 adAccountBalance = (AdAccountBalance, "")
 
 adAccountSpent :: (AdAccountSpent, Text)
 adAccountSpent = (AdAccountSpent, "")
 
-adAccountAge :: (AdAccountAge, Float)
-adAccountAge = (AdAccountAge, 3.14159)
+adAccountAge :: (AdAccountAge, Double)
+adAccountAge = (AdAccountAge, pi)
 
 adAccountId = (FId, Id "")
 adAccountAccId = (FAdAccountId, Id "")
+
+adAccountGroups :: (AdAccountGroups, V.Vector AAGP.AdAccountGroupResult)
+adAccountGroups = (AdAccountGroups, V.fromList [AAGP.testGroup])
+
+adAccountFSD :: (FSD, FundingSourceDetails)
+adAccountFSD = (FSD, fsd)
+
+adAccountACD :: (AdAccountACD, AgencyClientDeclaration)
+adAccountACD = (AdAccountACD, AgencyClientDeclaration 0)
+
+fsd = (FSD_Id, Id "") :*: (FSD_Type, 0) :*: (FSD_DisplayString, "") :*: Nil
 
 data AdAccountSpent = AdAccountSpent
 instance Field AdAccountSpent where
@@ -234,7 +274,7 @@ instance Field AdAccountSpent where
     fieldLabel = AdAccountSpent
 -- TODO
 
-testRec = (FId, "TEST_ID") :*: (FSD, FS_Coupon) :*: Nil
+--testRec = (FId, "TEST_ID") :*: (FSD, FS_Coupon) :*: Nil
 getter rec = (rec `get` FId, rec `get` FSD)
 builder fid fsd = (FId, fid) :*: (FSD, fsd) :*: Nil
 
@@ -245,13 +285,26 @@ getAdAccountId token = getObject "/v2.5/me/adaccounts" [] (Just token)
 
 acc_id rec = rec `get` FId
 acc_acc_id rec = rec `get` FAdAccountId
+-- TODO: gen all other accesors
 
 instance ToFbText Id where
     toFbText (Id id) = id
 
-type AdAccC r = (Has FId r, Has FAdAccountId r)
+class IsAdAccountField rec
+instance (IsAdAccountField h, IsAdAccountField t) => IsAdAccountField (h :*: t)
+instance IsAdAccountField Nil
+instance IsAdAccountField FId
+instance IsAdAccountField FAdAccountId
+instance IsAdAccountField AdAccountGroups
+instance IsAdAccountField FSD
+instance IsAdAccountField AdAccountBalance
+instance IsAdAccountField AdAccountSpent
+instance IsAdAccountField AdAccountAge
+-- TODO: add other fields
 
-getAdAccount :: (R.MonadResource m, MonadBaseControl IO m, AdAccC rec, ToBS rec, A.FromJSON rec) =>
+type AdAcc r = (Has FId r, Has FAdAccountId r, ToBS r, A.FromJSON r, IsAdAccountField r)
+
+getAdAccount :: (R.MonadResource m, MonadBaseControl IO m, AdAcc rec) =>
            Id -- AdAccountId    -- ^ Ad Account Id
         -> [(BS.ByteString, rec)]     -- ^ Arguments to be passed to Facebook.
         -> Maybe UserAccessToken -- ^ Optional user access token.
