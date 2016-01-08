91b71974b41d24420dead8857952fc0a230c4f8a
Alex Gremm <alex@beautifuldestinations.com>
2016-01-08 09:11:31 +0000

    Fix AdSet creation

--- src/Facebook/Object/Marketing/TargetingSpecs.hs
+++ src/Facebook/Object/Marketing/TargetingSpecs.hs
@@ -4,28 +4,39 @@ module Facebook.Object.Marketing.TargetingSpecs where
 
 import Data.Text (Text, unpack, pack)
 import Data.Aeson
+import Data.Aeson.Types
 import Data.Default
 --import Facebook.Object.Marketing.Types
+import GHC.Generics (Generic)
 import Facebook.Records
 import Facebook.Object.Marketing.TargetingSpecs.Demographies
 import Facebook.Object.Marketing.TargetingSpecs.Location
 import Facebook.Object.Marketing.TargetingSpecs.Mobile
 import Facebook.Object.Marketing.TargetingSpecs.Placement
+import qualified Data.ByteString.Lazy as BL
 
 data TargetingSpecs = TargetingSpecs
-  { exclusions :: Value
-  , geo_location :: Maybe TargetLocation
-  , mobile_targeting :: Maybe MobileTargeting -- ^ Please note that the fields of this record will be at the top level
-  , page_types :: [PlacementOption]
-  } deriving (Show, Eq)
+  { --exclusions :: Maybe Value
+   geo_locations :: TargetLocation
+  --, mobile_targeting :: Maybe MobileTargeting -- ^ Please note that the fields of this record will be at the top level
+  --, page_types :: Maybe [PlacementOption]
+  } deriving (Show, Eq, Generic)
 
 instance ToJSON TargetingSpecs where -- FIXME
-  toJSON (TargetingSpecs exclusions geo mobile placement) =
-      let Object m = toJSON mobile
-          fieldsExcludingMobile = undefined
-      in undefined m fieldsExcludingMobile
+    toJSON = genericToJSON defaultOptions { omitNothingFields = True }
+  --toJSON (TargetingSpecs exclusions geo mobile placement) =
+  --    let Object m = toJSON mobile
+  --        fieldsExcludingMobile = undefined
+  --    in undefined m fieldsExcludingMobile
 
 instance FromJSON TargetingSpecs where
     parseJSON (Object v) = undefined
 
-instance ToBS TargetingSpecs
+instance ToBS TargetingSpecs where
+    toBS a = toBS $ toJSON a
+
+instance ToBS TargetLocation where
+    toBS = toBS . toJSON
+
+instance ToBS Value where
+    toBS = BL.toStrict . encode
